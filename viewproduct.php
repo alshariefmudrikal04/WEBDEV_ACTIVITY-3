c70b848f130f6dc5fbffec6f266aa2acf5fca1c3
AL <alshariefmudrikal04@Gmail.com>
2025-09-02 09:49:57 +0800

    Second Commmit

--- viewproduct.php
+++ viewproduct.php
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Product Info</title>
 </head>
 <body>
-    
+    <h2>Product Information</h2>
+
+    <label>Product Name: <?php echo $_POST["product_name"]; ?></label><br>
+    <label>Category: <?php echo $_POST["category"]; ?></label><br>
+    <label>Price: ₱<?php echo number_format($_POST["price"], 2); ?></label><br>
+    <label>Stock Quantity: <?php echo $_POST["stock_quantity"]; ?></label><br>
+    <label>Expiration Date: <?php echo $_POST["expiration_date"]; ?></label><br>
+    <label>Status: <?php echo $_POST["status"]; ?></label><br>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
->>>>>>> 4d1d2fff6eff5d27f573a8543bde4a937cedc2c9
 </html>
